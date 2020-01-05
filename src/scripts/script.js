9808a518131536b78909a31dbb1785435b634188
Jagoda Bodnar <jagodabodnar90@gmail.com>
2020-01-05 17:08:06 +0100

    correction

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,10 +1,10 @@
 
   
-var prefix = "https://cors-anywhere.herokuapp.com/";
+const prefix = "https://cors-anywhere.herokuapp.com/";
 
-var tweetLink = "https://twitter.com/intent/tweet?text=";
+const tweetLink = "https://twitter.com/intent/tweet?text=";
 
-var quoteUrl = "https://quotesondesign.com/wp-json/wp/v2/posts/?orderby=rand";
+const quoteUrl = "https://quotesondesign.com/wp-json/wp/v2/posts/?orderby=rand";
 
 
 function getQuote() {
@@ -19,25 +19,25 @@ function getQuote() {
 }
 
 function createTweet(input) {
-    var data = input[Math.ceil(Math.random() * 10)]; 
+    let data = input[Math.ceil(Math.random() * 10)]; 
 
-    var dataElement = document.createElement('div');
+    let dataElement = document.createElement('div');
     dataElement.innerHTML = data.content.rendered;
-    var quoteText = dataElement.innerText.trim();
-    var quoteAuthor = data.title.rendered;
+    let quoteText = dataElement.innerText.trim();
+    let quoteAuthor = data.title.rendered;
 
     if (!quoteAuthor.length) {
         quoteAuthor = "Unknown author";
     }
 
-    var tweetText = "Quote of the day - "  + quoteText + " Author: " + quoteAuthor;
+    let tweetText = "Quote of the day - "  + quoteText + " Author: " + quoteAuthor;
 
     if (tweetText.length > 140) {
     	getQuote();
 	}
 
 	else {	
-	    var tweet = tweetLink + encodeURIComponent(tweetText);	   
+	    let tweet = tweetLink + encodeURIComponent(tweetText);	   
 	    document.querySelector('.quote').innerText = quoteText;	   
 	    document.querySelector('.author').innerText = "Author: " + quoteAuthor;
 	    document.querySelector('.tweet').setAttribute('href', tweet);
